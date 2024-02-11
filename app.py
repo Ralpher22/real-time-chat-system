f661481cd145dea6cafa9f0bafa9d126aec72401
Ralpher22 <rickyralph116@gmail.com>
2024-02-11 16:10:41 +0100

    created templates

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import join_room, leave_room, send, socketIO
+import random
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hjhjsdahhds'
+socketio = socketIO(app)
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+return render_template('home.html')
+
+if __name__ == '__main__':
+  socketio.run(app, debug=True)
